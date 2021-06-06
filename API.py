1500d33aa5c1d7ce55c7c7fcbdec583169505b38
Victor Javier Aguayo Mendoza <a01229260@itesm.mx>
2021-06-06 15:29:18 -0500

    Fix model trainning
    Ahora entrenamos nuestrro modelo en la creacion del API y lo pasamos como parametro a nuestras funciones de modelo

--- API.py
+++ API.py
@@ -5,18 +5,21 @@ import model
 import requests
 import os
 
+modelo=None
 
 def create_app():
     app = Flask(__name__)
+    #Trainning model
+    global modelo
+    modelo=model.modelTraining()
     return app
-
 app = create_app()
 
 @app.route('/api/pothole/', methods=['POST'])
 def verify_pothole():
     data = request.get_json()
     windows = model.createWindows(data) 
-    locations = model.potholeOrNotPothole(windows)
+    locations = model.potholeOrNotPothole(modelo,windows)
     response = {'locations': locations}
     api_crud = os.getenv('API_CRUD')
     r_api_crud =requests.post(api_crud, data=response)
